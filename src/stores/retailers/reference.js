31c6843b8f0e77b4039560da8fac10f652927333
Juan Sierra <juan@totally.group>
2023-09-14 16:20:43 +0200

    Set default time to 3 months and split reference store for resellers and retailers

---
+++ src/stores/retailers/reference.js
@@ -0,0 +1,58 @@
+import { defineStore } from 'pinia';
+import { api } from 'boot/axios';
+
+export const useRetailersReferenceStore = defineStore(
+  'retailersReference',
+  {
+    persist: true,
+    state: () => {
+      return {
+        brands: [],
+        countries: [],
+        is: {
+          fetchingBrands: false,
+          fetchingCountries: false,
+        },
+      };
+    },
+    getters: {},
+    actions: {
+      fetchBrands() {
+        return new Promise((resolve, reject) => {
+          this.is.fetchingBrands = true;
+          api
+            .get('retailers/reporting/ref/brands')
+            .then((response) => {
+              this.brands = response.data.data;
+              resolve(response);
+            })
+            .catch((error) => {
+              this.brands = [];
+              reject(error);
+            })
+            .finally(() => {
+              this.is.fetchingBrands = false;
+            });
+        });
+      },
+      fetchCountries() {
+        return new Promise((resolve, reject) => {
+          this.is.fetchingCountries = true;
+          api
+            .get('retailers/reporting/ref/countries')
+            .then((response) => {
+              this.countries = response.data.data;
+              resolve(response);
+            })
+            .catch((error) => {
+              this.countries = [];
+              reject(error);
+            })
+            .finally(() => {
+              this.is.fetchingCountries = false;
+            });
+        });
+      },
+    },
+  }
+);
